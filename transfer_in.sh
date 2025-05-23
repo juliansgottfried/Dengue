8487fedae66bd370bf80d882a6949e81a0a44de1
Julian Gottfried <juliansgottfried@gmail.com>
2025-05-23 15:41:51 -0400

    dir creation

--- transfer_in.sh
+++ transfer_in.sh
@@ -1,7 +1,11 @@
 local_path=/Users/juliangottfried/Desktop/dengue_github/Dengue
-save_path=/Users/juliangottfried/Desktop/NYU/thailand/cluster_runs
+store_path=/Users/juliangottfried/Desktop/NYU/thailand/cluster_runs
+
+if [ ! -d $local_path/folders_for_fit ]; then
+	mkdir $local_path/folders_for_fit
+fi
 
 args=("$@")
 for arg in "${args[@]}"; do
-	cp $save_path/$arg $local_path/folders_for_fit
+	cp $store_path/$arg $local_path/folders_for_fit
 done
