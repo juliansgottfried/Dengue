22ec7f5bd6b16631062292e1e0f24963d07d0bb3
Julian Gottfried <juliansgottfried@gmail.com>
2025-05-23 02:12:16 -0400

    help

--- clean.sh
+++
@@ -1,4 +0,0 @@
-read -p "Press enter to confirm: "
-rm -r /scratch/$USER/out/*/* /scratch/$USER/folders_for_fit/*
-echo "Cleaned"
-
--- collate.sh
+++
@@ -1,4 +0,0 @@
-module unload r
-module unload gcc
-module load r/gcc/4.3.1
-Rscript /scratch/$USER/helpers/collate.R
