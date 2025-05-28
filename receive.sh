102428492243a5a07a26609055e8a49faba33c4a
Julian Gottfried <juliansg@princeton.edu>
2025-05-28 14:34:05 -0400

    improve transfer

---
+++ receive.sh
@@ -0,0 +1,2 @@
+ssh-keygen -R dtn.hpc.nyu.edu
+scp jg8461@dtn.hpc.nyu.edu:/scratch/jg8461/Dengue/run_times.txt .
---
+++ send.sh
@@ -0,0 +1,2 @@
+ssh-keygen -R dtn.hpc.nyu.edu
+scp send.sh jg8461@dtn.hpc.nyu.edu:/scratch/jg8461/Dengue
