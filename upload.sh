58148de22d259532ce690e25864c297773ce5640
Julian Gottfried <juliansgottfried@gmail.com>
2025-05-28 15:50:43 -0400

    fix

--- upload.sh
+++ upload.sh
@@ -6,8 +6,8 @@ source path_vars.sh
 
 names=($(cat $local_path/run_names.txt))
 
+ssh-keygen -R dtn.hpc.nyu.edu
 for name in "${names[@]}"; do
-	ssh-keygen -R dtn.hpc.nyu.edu
-	scp -o "StrictHostKeyChecking no" $store_path/$name jg8461@dtn.hpc.nyu.edu:/scratch/jg8461/Dengue/folders_for_fit
+	scp -or "StrictHostKeyChecking no" $store_path/$name jg8461@dtn.hpc.nyu.edu:/scratch/jg8461/Dengue/folders_for_fit/
 #	cp -r $store_path/$name $local_path/folders_for_fit
 done
