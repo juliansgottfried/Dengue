6a3277e28a06715f6bca298a1dffdd6c95bf579f
Julian Gottfried <juliansgottfried@gmail.com>
2025-05-28 17:58:32 -0400

    fix slurm maker

--- set_up.sh
+++ set_up.sh
@@ -8,7 +8,7 @@ mkdir /scratch/$USER/Dengue/folders_for_fit
 
 read -p "Enter email: " email_address
 echo "#SBATCH --mail-user=$email_address" > /scratch/$USER/Dengue/helpers/parts/tmp.txt
-cat /scratch/$USER/Dengue/helpers/parts/top.txt /scratch/$USER/Dengue/helpers/parts/tmp.txt /scratch/$USER/Dengue/helpers/parts/bottom.txt > /scratch/$USER/Dengu>
+cat /scratch/$USER/Dengue/helpers/parts/top.txt /scratch/$USER/Dengue/helpers/parts/tmp.txt /scratch/$USER/Dengue/helpers/parts/bottom.txt > /scratch/$USER/Dengue/helpers/run.slurm
 rm /scratch/$USER/Dengue/helpers/parts/tmp.txt
 
 module unload r
