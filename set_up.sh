2c306fec5303e0a8ca672215ec7d316e59396e41
Julian Gottfried <juliansgottfried@gmail.com>
2025-05-28 17:32:06 -0400

    updates

--- set_up.sh
+++ set_up.sh
@@ -6,6 +6,11 @@ mkdir /scratch/$USER/Dengue/out/output
 
 mkdir /scratch/$USER/Dengue/folders_for_fit
 
+read -p "Enter email: " email_address
+echo "#SBATCH --mail-user=$email_address" > /scratch/$USER/Dengue/helpers/parts/tmp.txt
+cat /scratch/$USER/Dengue/helpers/parts/top.txt /scratch/$USER/Dengue/helpers/parts/tmp.txt /scratch/$USER/Dengue/helpers/parts/bottom.txt > /scratch/$USER/Dengu>
+rm /scratch/$USER/Dengue/helpers/parts/tmp.txt
+
 module unload r
 module unload gcc
 module load r/gcc/4.3.1
