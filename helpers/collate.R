9882958bfcfdda1d29d3bcb16e3c82400b24e8f7
Julian Gottfried <juliansgottfried@gmail.com>
2025-07-11 20:46:15 -0400

    automatically process

--- helpers/collate.R
+++ helpers/collate.R
@@ -13,6 +13,14 @@ process<-function(path) {
 		mutate_all(as.numeric)
 	}
 
+summary_data <- c(rep(NA,5))
+names(summary_data) <- c("run","time","loglik","k","aic")
+
+source(paste0(path,"object.R"))
+pars_path <- paste0(path,"pars.csv")
+init_vals <- read_csv(pars_path)
+est_pars <- par_names[apply(init_vals,2,function(x) {max(x)-min(x)})!=0]
+
 lapply(data,function(type) {
     print(paste0(toupper(type)))
 
@@ -23,6 +31,9 @@ lapply(data,function(type) {
         print(paste0(name))
 
 	source(paste0(path,"object.R"))
+	
+	init_vals <- read_csv(paste0(path,"pars.csv"))
+	k <- length(par_names[apply(init_vals,2,function(x) {max(x)-min(x)})!=0])
 
         files<-list.files(path,full.names=T)
         accum<-process(files[1])
@@ -31,6 +42,17 @@ lapply(data,function(type) {
 		add<-process(files[i])
 		accum<-bind_rows(accum,add)
 	}
+
+	if (type=="results") {
+		maxlik <- accum %>% arrange(-loglik) %>% pull(loglik) %>% head(1)
+		aic <- 2*(k-maxlik)
+		current <- c(name,NA,maxlik,k,aic)
+		names(current) <- names(summary_data)
+		summary_data <- bind_rows(summary_data,current)	
+	}
+
+	summary_data <- summary_data %>% na.omit(cols=run)
+	write_csv(summary_data,paste0(path_name,"/folders_for_fit/summary.csv"))
 
         write_csv(accum,paste0(path_name,"/folders_for_fit/",name,"/",type,".csv"))
     })
